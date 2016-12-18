f0b12bd52f497732ee52323c23d8077407b86ff7
Joel Santos <jsantos.redhat@gmail.com>
2016-12-18 10:48:37 -0300

    Alteração da configuração.

--- config.h
+++ config.h
@@ -9,8 +9,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Noto Mono:size=8:antialias=true" };
-static const char dmenufont[]       = "Noto Mono:size=8:antialias=true";
+static const char *fonts[]          = { "Source Sans Pro:size=9:antialias=true" };
+static const char dmenufont[]       = "Source Sans Pro:size=9:antialias=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -42,9 +42,9 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[M]",      monocle },
+	{ "#",      tile },    /* first entry is default */
+	{ "@",      NULL },    /* no layout function means floating behavior */
+	{ "M",      monocle },
 };
 
 /* key definitions */
@@ -62,11 +62,21 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "xfce4-terminal", NULL };
+static const char *mutecmd[] = {"amixer","-D","pulse","sset","Master","toggle",NULL};
+static const char *lowervolcmd[] = {"amixer","-D","pulse","sset","Master","3%-",NULL};
+static const char *raisevolcmd[] = {"amixer","-D","pulse","sset","Master","3%+",NULL};
+static const char *lowerlightcmd[] = {"xbacklight","-dec","10",NULL};
+static const char *raiselightcmd[] = {"xbacklight","-inc","10",NULL};
+static const char *lockcmd[] = {"slock",NULL};
+static const char *mediaplay[] = {"playerctl","play-pause",NULL};
+static const char *medianext[] = {"playerctl","next",NULL};
+static const char *mediaprev[] = {"playerctl","previous",NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lockcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -88,6 +98,14 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+    { 0,                            0x1008ff14,spawn,          {.v = mediaplay } },
+    { 0,                            0x1008ff17,spawn,          {.v = medianext } },
+    { 0,                            0x1008ff16,spawn,          {.v = mediaprev } },
+    { 0,                            0x1008ff12,spawn,          {.v = mutecmd } },
+    { 0,                            0x1008ff11,spawn,          {.v = lowervolcmd } },
+    { 0,                            0x1008ff13,spawn,          {.v = raisevolcmd } },
+    { 0,                            0x1008ff03,spawn,          {.v = lowerlightcmd } },
+    { 0,                            0x1008ff02,spawn,          {.v = raiselightcmd } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
